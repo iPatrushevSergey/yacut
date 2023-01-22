610715fa8e1d14543deee81c52a0b9e5ce62b35b
iPatrushevSergey <PatrushevSergeyVal@yandex.ru>
2023-01-22 19:47:07 +0300

    form

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,3 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField, URLField
+from wtforms import StringField, SubmitField, URLField
+from wtforms.validators import DataRequired, Length, Optional, URL
 
+
+class URLMapForm(FlaskForm):
+    original_link = URLField(
+        'Enter the original long link',
+        validators=[
+            DataRequired(message='Required field'),
+            URL(message='Invalid URL entered'),
+        ],
+    )
+    custom_id = StringField(
+        'Enter a short ID',
+        validators=[
+            Length(1, 16),
+            Optional(),
+        ],
+    )
+    submit = SubmitField('Add')
