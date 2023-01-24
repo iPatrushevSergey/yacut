9bb872128f46ae219bc3ee4cf47b144fa978544d
iPatrushevSergey <PatrushevSergeyVal@yandex.ru>
2023-01-24 13:33:59 +0300

    url path

--- yacut/views.py
+++ yacut/views.py
@@ -1,3 +1,6 @@
+from random import choices
+from string import ascii_letters, digits
+
 from flask import redirect, render_template, url_for
 
 from yacut import app, db
@@ -5,15 +8,31 @@ from yacut.forms import URLMapForm
 from yacut.models import URLMap
 
 
+def get_unique_short_id(length):
+    return ''.join(choices(ascii_letters + digits, k=length))
+
+
 @app.route('/', methods=['GET', 'POST'])
 def url_clipping_view():
     form = URLMapForm()
     if form.validate_on_submit():
-        url = URLMap(
+        protocol_domain_suffix = url_for('url_clipping_view', _external=True)
+        if not form.custom_id.data:
+            path = get_unique_short_id(6)
+        else:
+            path = form.custom_id.data
+        short_url = protocol_domain_suffix + path
+        combined_url = URLMap(
             original=form.original_link.data,
-            short=form.custom_id.data
+            short=short_url
         )
-        db.session.add(url)
+        db.session.add(combined_url)
         db.session.commit()
         return redirect(url_for('url_clipping_view'))
     return render_template('cut.html', form=form)
+
+
+@app.route('/<path:short>')
+def redirect_view(short):
+    url = URLMap.query.filter(short=short)
+    return redirect(url.original)
