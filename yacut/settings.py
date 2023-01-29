ebe4856aa9eecf040e0bf56d830eb8bc7cd8e8e1
iPatrushevSergey <PatrushevSergeyVal@yandex.ru>
2023-01-30 02:23:16 +0300

    default value

--- yacut/settings.py
+++ yacut/settings.py
@@ -5,6 +5,6 @@ class Config(object):
     """
     Configures basic settings.
     """
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
